ed9fe13421815462cd147c1904906edfb2870f80
Functionup-mn <nadeem2mau@gmail.com>
2023-08-23 16:44:22 +0530

    I have to make front page of hero brand

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+       <nav className='container'>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="" />
+        </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
